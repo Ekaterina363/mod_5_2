d8e3fcfd473061c3b73e6e9e57477167ccb8118e
Ekaterina363 <batsl363@gmail.com>
2024-12-19 17:38:22 +0400

    Add files via upload

---
+++ mod_5_2.py
@@ -0,0 +1,25 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to (self,new_floor):
+        if new_floor in range(1, self.number_of_floors + 1):
+            return new_floor
+        else:
+            return "Такого этажа не существует"
+    def __len__(self):
+        return self.number_of_floors
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
+h1 = House("ЖК Эльбрус", 10)
+h2 = House("ЖК Акация", 20)
+print(h1.go_to(11))
+print(h2.go_to(20))
+print(h1)
+print(h2)
+print(len((h1)))
+print(len((h2)))
+
+
+
+
